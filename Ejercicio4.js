82c831449ad6554bf9b496057ca029554239da8b
Jorge <newdarkjg@gmail.com>
2023-09-18 11:44:40 +0200

    Ejercicio4

---
+++ Ejercicio4.js
@@ -0,0 +1,8 @@
+var laLigaGoals=43; # line 1 <- error on this line, see below for error message
+var championsLeagueGoals=10;
+var copaDelReyGoals=5;
+console.log(suma(laLigaGoals,championsLeagueGoals,copaDelReyGoals));
+function suma (a,b,c){
+    var total = a+b+c;
+    return total;
+    }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Ejercicio 1 :https://www.codewars.com/kata/555086d53eac039a2a000083
 Ejercicio 2 :https://www.codewars.com/kata/53ee5429ba190077850011d4
 Ejercicio 3: https://www.codewars.com/kata/5772da22b89313a4d50012f7
+Ejercicio 4: https://www.codewars.com/kata/55ca77fa094a2af31f00002a
