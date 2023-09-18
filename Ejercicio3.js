c5ef1467102918ab4015219a03cfade35a38d34f
Jorge <newdarkjg@gmail.com>
2023-09-18 10:44:48 +0200

    Ejercicio3

---
+++ Ejercicio3.js
@@ -0,0 +1,20 @@
+var nombre
+const propietario ="Jorge";
+
+console.log (Nombre("Jorge"))
+
+
+
+
+function Nombre(nombre){
+  var devuelto;
+if (nombre == propietario){
+  devuelto= "Hola Boos"
+  return devuelto;
+  }else{
+    devuelto = "Hola invitado"
+    return devuelto ;
+    
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Ejercicios-Katas
 Ejercicio 1 :https://www.codewars.com/kata/555086d53eac039a2a000083
 Ejercicio 2 :https://www.codewars.com/kata/53ee5429ba190077850011d4
+Ejercicio 3: https://www.codewars.com/kata/5772da22b89313a4d50012f7
